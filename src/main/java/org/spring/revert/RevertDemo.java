9fd8c46f138303743613dd95bd4c115622cca5eb
Zhyldyz <zhyldyz9494@gmail.com>
2026-07-21 10:52:43 +0600

    BUg has been fixed

--- src/main/java/org/spring/revert/RevertDemo.java
+++ src/main/java/org/spring/revert/RevertDemo.java
@@ -3,5 +3,6 @@ package org.spring.revert;
 public class RevertDemo {
     public static void main(String[] args) {
         System.out.println("This line has a bug!!!");
+        System.out.println("Bug has been fixed!!");
     }
 }
