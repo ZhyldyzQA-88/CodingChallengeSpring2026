3e13ec82665449a8e0af0e6b85c4efb472aaf0cd
Zhyldyz <zhyldyz9494@gmail.com>
2026-07-22 09:41:57 +0600

    Hello world

--- src/main/java/org/spring/conflict/ConflictClass.java
+++ src/main/java/org/spring/conflict/ConflictClass.java
@@ -2,6 +2,6 @@ package org.spring.conflict;
 
 public class ConflictClass {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
     }
 }
